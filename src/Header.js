fdd0512dd63de65771cb68f6acb5df820614abd7
[udamadu11] <madu12dara@gmail.com>
2021-05-07 10:44:59 +0530

    register api integrate

--- src/Header.js
+++ src/Header.js
@@ -6,13 +6,13 @@ return(
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">FoodX</Navbar.Brand>
             <Nav className="mr-auto">
-                <Link class="nav-link active" to="/home">Home</Link>
-                <Link class="nav-link active" to="/features">Features</Link>
-                <Link class="nav-link active" to="/pricing">Pricing</Link>
+                <Link className="nav-link active" to="/home">Home</Link>
+                <Link className="nav-link active" to="/features">Features</Link>
+                <Link className="nav-link active" to="/pricing">Pricing</Link>
             </Nav>
             <Nav>
-                <Link class="nav-link active" to="/login">Login</Link>
-                <Link class="nav-link active" to="/register">Register</Link>
+                <Link className="nav-link active" to="/login">Login</Link>
+                <Link className="nav-link active" to="/register">Register</Link>
             </Nav>
             
         </Navbar>
--- src/Register.js
+++ src/Register.js
@@ -1,18 +1,60 @@
-import {} from 'react-bootstrap';
-import React from 'react';
+import React,{ useState } from 'react';
+import { Button } from 'react-bootstrap';
 function Register(){
+    const[first_name,setFirstName] = useState('');
+    const[last_name,setLastName] = useState('');
+    const[address,setAddress] = useState('');
+    const[email,setEmail] = useState('');
+    const[password,setPassword] = useState('');
+
+    async function signUp(){
+        let item = {first_name,last_name,address,email,password};
+        console.log(item);
+        let result = await fetch('http://localhost:8000/api/register',{
+            method:"POST",
+            body: JSON.stringify(item),
+            headers:{
+                "Content-Type": "application/json",
+                "Accept" : "application/json"
+            }
+        })
+
+        result = await result.json();
+        console.log("result",result);
+    }
+
     return(
-        <div class="container">
-            <div class="card ">
-            <div class="card-body">
-                <div class="row g-3 col-md-6">
-                    <div class="col">
-                        <input type="text" class="form-control" placeholder="First name" aria-label="First name" />
+        <div className="container">
+            <div className="card col-sm-8 card-c offset-sm-2">
+            <div className="card-body">
+                    <div className="row g-3">
+                        <div className="col">
+                            <input type="text" className="form-control" name="first_name" value={first_name} onChange={(e)=>setFirstName(e.target.value)} placeholder="First name" />
+                        </div>
+                        <div className="col">
+                            <input type="text" className="form-control" name="last_name" value={last_name} onChange={(e)=>setLastName(e.target.value)} placeholder="Last name" />
+                        </div>
                     </div>
-                    <div class="col">
-                        <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" />
+                    <div className="row g-3">
+                        <div className="col col-a">
+                            <input type="text" className="form-control" name="address" value={address} onChange={(e)=>setAddress(e.target.value)} placeholder="Address"/>
+                        </div>
                     </div>
-                </div>
+                    <div className="row g-3">
+                        <div className="col col-a">
+                            <input type="email" className="form-control" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email"/>
+                        </div>
+                    </div>
+                    <div className="row g-3">
+                        <div className="col col-a">
+                            <input type="password" className="form-control" name="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password"/>
+                        </div>
+                    </div>
+                    <div className="row g-3">
+                        <div className="col col-a">
+                            <Button className="btn  btn-success" onClick={signUp}>Register</Button>
+                        </div>
+                    </div>
             </div>
         </div>
         </div>
